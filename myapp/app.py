1247a16341ca44ff202118784433a86068aef03e
ZabeidaNazar <nazarzabeida@gmail.com>
2026-07-12 18:52:52 +0300

    added more operations added failing tests for division operation (incorrect operation) test coverage < 70%

--- myapp/app.py
+++ myapp/app.py
@@ -4,3 +4,18 @@ def add(a, b):
 
 def substruct(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a - b
+
+
+def power(a, b):
+    res = 1
+    for _ in range(b):
+        res *= a
+    return res
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,25 @@
-from myapp.app import add, substruct
+import pytest
+from myapp.app import add, substruct, divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_substruct():
     assert substruct(5, 3) == 2
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (9, 3, pytest.approx(3)),
+        (9, 5, pytest.approx(1.8)),
+        (9, 6, pytest.approx(1.5)),
+        (9, 7, pytest.approx(1.285714286)),
+        (0, 7, pytest.approx(0)),
+    ]
+)
+def test_divide(a, b, expected):
+    assert divide(a, b) == expected
+
+def test_divide_0():
+    with pytest.raises(ZeroDivisionError):
+        divide(9, 0)
